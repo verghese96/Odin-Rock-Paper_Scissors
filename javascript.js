07d76e5aa1842d29b43a2ec5fa75212c1a1415c2
P A Verghese <84585485+verghese96@users.noreply.github.com>
2023-02-27 11:52:57 +0800

    Completed User Function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,40 @@
 console.log("Hello World");
+
+var playerSelection = prompt()
+
+function checkPlayerSelection () {
+    playerSelection = playerSelection.toLocaleLowerCase();
+
+    if (playerSelection== "scissors" ){
+        return playerSelection;
+    }
+    else if (playerSelection=="rock"){
+        return playerSelection;
+    }
+    else if (playerSelection=="paper"){
+        return playerSelection;
+    }
+    else{
+        console.log ('Please enter either Rock, Paper, Scissors!');
+        }
+
+    }
+
+
+var computerSelection;
+
+function getComputerChoice(computerSelection){
+    var x = ["scissors","paper","rock"];
+    var  i = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    var computerSelection = x[i];
+    return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+
+  checkPlayerSelection();
+//   getComputerChoice();
+  console.log(playerSelection);
+//   console.log(computerSelection);
