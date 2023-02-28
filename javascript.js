986c31f878de14088604e7bc95b30389e273f20a
verghese96 <verghese96@gmail.com>
2023-02-28 11:05:30 +0800

    Completed playRound function

--- javascript.js
+++ javascript.js
@@ -21,26 +21,58 @@ function checkPlayerSelection () {
 var computerSelection = Math.floor((Math.random()*3)+1);
 
 function getComputerChoice(){
-    if (computerSelection = 1){
+    if (computerSelection == 1){
         computerSelection = "scissors";
         return computerSelection;
     }
-    else if (computerSelection= 2){
+    else if (computerSelection == 2){
         computerSelection = "paper";
         return computerSelection;
     }
-    else if (computerSelection= 3){
+    else if (computerSelection == 3){
         computerSelection = "rock";
         return computerSelection;
     }
   }
 
-
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log ("Congrats! You win!"); 
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log ("No.. You lost!"); 
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log ("No.. You lost!"); 
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log ("Congrats! You win!");
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log ("Congrats! You win!");
+    }     
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log ("No.. You lost!"); 
+    } 
+    else if (playerSelection == computerSelection){
+        console.log ("So close... it was a tie!"); 
+    }
+    else {
+        console.log("did you input correctly?");
+    }
   }
 
-  checkPlayerSelection();
-  getComputerChoice();
-  console.log(playerSelection);
-  console.log(computerSelection);
+// function game(){
+//     for (let i = 0; i < 5; i++) {
+//         checkPlayerSelection();
+//         getComputerChoice();
+//         result = playRound(playerSelection, computerSelection;
+//         console.log(`In round ${i}, ${result}`)
+//      }
+// }
+
+checkPlayerSelection();
+getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
