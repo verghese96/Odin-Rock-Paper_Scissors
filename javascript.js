b4051dcef313bfa34fea2fc1c8ac52575ef94fad
verghese96 <verghese96@gmail.com>
2023-02-28 23:13:15 +0800

    Completed Game Function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
-var playerSelection = prompt("Please enter your choice?")
+var playerSelection;
+var computerSelection;
+var result;
 
 function checkPlayerSelection () {
     playerSelection = playerSelection.toLocaleLowerCase();
 
     if (playerSelection=="scissors" ){
         return playerSelection;
-    }
+    }   
     else if (playerSelection=="rock"){
         return playerSelection;
     }
@@ -14,65 +16,67 @@ function checkPlayerSelection () {
     }
     else{
         console.log ('Please enter either Rock, Paper, Scissors!');
-        }
-
+        return;
     }
 
-var computerSelection;
+    }
 
 function getComputerChoice(){
     computerSelection = Math.floor((Math.random()*3)+1);
-    if (computerSelection == 1){
-        computerSelection = "scissors";
-        return computerSelection;
-    }
-    else if (computerSelection == 2){
-        computerSelection = "paper";
-        return computerSelection;
-    }
-    else if (computerSelection == 3){
-        computerSelection = "rock";
-        return computerSelection;
+    switch (computerSelection) {
+        case 1:
+            computerSelection = "scissors";
+            return computerSelection;
+        case 2:
+            computerSelection = "paper";
+            return computerSelection;
+        case 3:
+            computerSelection = "rock";
+            return computerSelection;
     }
   }
 
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log ("Congrats! You win!"); 
+        return result = "Congrats! You win!"; 
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log ("No.. You lost!"); 
+        return result = "No.. You lost!"; 
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log ("No.. You lost!"); 
+        return result = "No.. You lost!"; 
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log ("Congrats! You win!");
+        return result = "Congrats! You win!";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log ("Congrats! You win!");
+        return result = "Congrats! You win!";
     }     
     else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log ("No.. You lost!"); 
+        return result = "No.. You lost!"; 
     } 
-    else if (playerSelection == computerSelection){
-        console.log ("So close... it was a tie!"); 
+    else if (playerSelection == computerSelection) {
+        return result = "So close... it was a tie!"; 
     }
     else {
-        console.log("did you input correctly?");
+        return result = "Your input was incorrect!"; 
     }
   }
 
 function game(){
     for (let i = 0; i < 5; i++) {
         playerSelection = prompt("Please enter your choice?")
+
         checkPlayerSelection();
         getComputerChoice();
-        playRound();
+        playRound(playerSelection, computerSelection);
+
         console.log("You have selected " + playerSelection);
         console.log("The computer has selected " +computerSelection);
-        console.log(`In round ${i+1}, ${playRound(playerSelection, computerSelection)}`)
+        console.log(`In round ${i+1}, ${result}`)
      }
 }
+
 
 game();
